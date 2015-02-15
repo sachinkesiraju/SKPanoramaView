199260527f36d8e09e0bb38fae4c9c45de07bd56
Sachin Kesiraju <kesirajusachin@gmail.com>
2015-02-15 20:20:16 +0530

    Added linkedin demo.

--- SKPanoramaViewDemo/SKPanoramaViewDemo/ViewController.m
+++ SKPanoramaViewDemo/SKPanoramaViewDemo/ViewController.m
@@ -18,6 +18,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib
+    [self showLinkedinAnimation];
+}
+
+- (void) showLinkedinAnimation
+{
+    SKPanoramaView *panoramaView = [[SKPanoramaView alloc] initWithFrame:self.view.frame image:[UIImage imageNamed:@"new york"]];
+    panoramaView.animationDuration = 100;
+    [self.view addSubview:panoramaView];
+    [panoramaView startAnimating];
+    UIView *overlayView = [[UIView alloc] initWithFrame:self.view.frame];
+    overlayView.backgroundColor = [UIColor blackColor];
+    overlayView.alpha = 0.4;
+    [panoramaView addSubview:overlayView];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"linkedin"]];
+    imageView.center = panoramaView.center;
+    [panoramaView addSubview:imageView];
+}
+
+- (void) showStandardAnimation
+{
     SKPanoramaView *panoramaView = [[SKPanoramaView alloc] initWithFrame:self.view.frame image:[UIImage imageNamed:@"golden gate"]];
     panoramaView.animationDuration = 10.0f; //Set the duration
     [self.view addSubview:panoramaView];
